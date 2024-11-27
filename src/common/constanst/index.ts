0c5671c5d509ef5727acc3638df96907ac1974d1
wayTami-O <weylant098@gmail.com>
2024-11-27 20:31:49 +0400

    constants pages

--- src/common/constanst/index.ts
+++ src/common/constanst/index.ts
@@ -1,2 +1,20 @@
 
 export const DEV_MODE = process.env.NODE_ENV === 'development';
+
+type links = {
+    main: string,
+    calculation: string,
+    projects: string,
+    team: string,
+    franchise: string,
+    contacts: string,
+}
+
+export const linksPage: links = {
+    main: '/',
+    calculation: '/calculation',
+    projects: '/projects',
+    team: '/team',
+    franchise: '/franchise',
+    contacts: '/contacts',
+}
