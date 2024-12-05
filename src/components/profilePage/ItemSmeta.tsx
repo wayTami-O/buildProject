abb330240d45658392e3d22c2ea5c36cac89ca52
brav1olly <wh4t1slove29@gmail.com>
2024-12-05 12:08:18 +0300

    fix

--- src/components/profilePage/ItemSmeta.tsx
+++ src/components/profilePage/ItemSmeta.tsx
@@ -6,7 +6,7 @@ function Smeta({title}: smetaConstructor) {
     return (
         <div className="flex gap-[1.313rem] items-center">
             <div className="w-[14rem] h-[4.063rem] flex justify-center items-center border-[0.063rem] border-yellow">
-                <p>Смета для Зал</p>
+                <p>{title}</p>
             </div>
         </div>
     )
